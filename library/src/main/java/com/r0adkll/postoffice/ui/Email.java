dafbbaba44285b4ab63185ecc1850d3b38bc7525
r0adkll <veedubusc@gmail.com>
2015-04-26 17:39:00 -0400

    Removed the old ripple view library infavor of just using ?attr/selectableItemBackground instead. Preparing for testing new API's

---
+++ library/src/main/java/com/r0adkll/postoffice/ui/Email.java
@@ -0,0 +1,22 @@
+package com.r0adkll.postoffice.ui;
+
+/**
+ * <p>
+ *     This is an experimental UI class for testing out the new Material Design enhancements to the
+ *     dialog system introduced in the newest Support Library Release.
+ * </p>
+ *
+ * <p>
+ *     Changes, features, and enhancements in this class will probably be rolled over to the Mail
+ *     class for the final release, it will probably just be refactored, so this class
+ *     is not to be relied upon in production
+ * </p>
+ *
+ *
+ * Created by r0adkll on 4/26/15.
+ */
+public class Email {
+
+
+
+}
